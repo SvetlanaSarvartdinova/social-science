d32b9b614e246338dffac6c2ee12bcc41be8b16c
SvetlanaSarvartdinova <svetasarvartdinova@gmail.com>
2020-12-17 22:09:33 +0300

    delete host

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '7j$7)+j9f#z8$u64n@gapk06sbv_uu
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['socialscience.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['socialscience.herokuapp.com']
 
 
 # Application definition
